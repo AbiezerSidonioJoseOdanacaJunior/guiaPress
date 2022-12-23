eb815fbcea6cef4eeec020fd70f529c341393ae9
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2022-12-23 20:31:08 -0300

    form category

--- categoreis/categoriesController.js
+++ categoreis/categoriesController.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/admin/categories/new", (req, res) => {
+  res.render("admin/categories/new");
+});
+
 router.get("/categorias", (req, res) => {
   res.send("Ola sou rota de categorias");
 });
