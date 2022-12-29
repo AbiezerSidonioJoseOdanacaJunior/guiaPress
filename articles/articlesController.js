f41b76b6ec11d9031835662f416d99dcace692c6
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2022-12-29 11:11:38 -0300

    Save articles

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categoreis/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
 router.get("/admin/articles", (req, res) => {
   res.render("admin/articles/index");
@@ -9,6 +11,19 @@ router.get("/admin/articles", (req, res) => {
 router.get("/admin/articles/new", (req, res) => {
   Category.findAll().then((categories) => {
     res.render("admin/articles/new", { categories: categories });
+  });
+});
+
+router.post("/articles/save", (req, res) => {
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category;
+
+  Article.create({
+    title: title,
+    slug: slugify(title),
+    body: body,
+    categoryId: category,
   });
 });
 
