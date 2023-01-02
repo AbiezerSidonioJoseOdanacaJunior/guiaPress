6d9d8ec2788ebcb8f883fcc76462e968c8b6944a
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2023-01-02 11:55:51 -0300

    Button delete Articles

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -33,4 +33,25 @@ router.post("/articles/save", (req, res) => {
   });
 });
 
+router.post("/articles/delete", (req, res) => {
+  var id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Article.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect("/admin/articles");
+      });
+    } else {
+      //Qudnfo não for um número
+      res.redirect("/admin/articles");
+    }
+  } else {
+    //Qudno for NULL
+    res.redirect("/admin/articles");
+  }
+});
+
 module.exports = router;
