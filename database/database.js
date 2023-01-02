0ce048443becf66e2ed37a784956a251518681f3
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2023-01-02 10:40:47 -0300

    Adjusting the time in Sequelize

--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ const sequelize = require("sequelize");
 const connection = new sequelize("blogPress", "root", "@bibi#14", {
   host: "localhost",
   dialect: "mysql",
+  timezone: "-03:00",
 });
 
 module.exports = connection;
